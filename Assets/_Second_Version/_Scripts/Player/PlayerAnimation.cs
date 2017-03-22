bce2615b2375835a421477860595ba517fd8403b
BerniceChua <Bernice.Chua.415@gmail.com>
2017-03-22 16:00:12 -0700

    Add character model, character animations, & run animations blend tree.

---
+++ Assets/_Second_Version/_Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+    //Animator m_animator;
+    Animator m_animator { get { return GetComponentInChildren<Animator>(); } set { m_animator = value; } }
+
+    private void Awake() {
+        //m_animator = GetComponentInChildren<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_animator.SetFloat("Vertical", GameManager.GameManagerInstance.InputController.m_vertical);
+        m_animator.SetFloat("Horizontal", GameManager.GameManagerInstance.InputController.m_horizontal);
+    }
+}
