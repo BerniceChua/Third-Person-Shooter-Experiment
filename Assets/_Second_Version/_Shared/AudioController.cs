7071996ac3c3f65ee210242a6da4641a814ceb5c
BerniceChua <Bernice.Chua.415@gmail.com>
2017-03-27 17:36:56 -0700

    Set up AudioController.cs.

---
+++ Assets/_Second_Version/_Shared/AudioController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour {
+
+    [SerializeField] AudioClip[] m_audClips;
+    [SerializeField] float m_timeDelayBetweenSounds;
+
+    bool m_canPlay;
+
+    //AudioSource m_audSource;
+    AudioSource m_audSource { get { return GetComponent<AudioSource>(); } set { m_audSource = value; } }
+
+    // Use this for initialization
+    void Start () {
+        //m_audSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play() {
+        if (!m_canPlay)
+            return;
+
+        // Resets m_canPlay to true again, once the m_timeDelayBetweenSounds has passed.
+        GameManager.GameManagerInstance.Timer.Add( () => {
+            m_canPlay = true; }, 
+            m_timeDelayBetweenSounds);
+
+        m_canPlay = false;
+
+        AudioClip selectedClip = Random.Range(0, m_audClips.Length);
+
+        // Plays the selected clip
+        m_audSource.PlayOneShot(selectedClip);
+    }
+
+}
