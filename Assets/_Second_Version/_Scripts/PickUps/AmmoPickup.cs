e39da484c5b3adf7dad7fb1df7c1bbe0b15041d6
BerniceChua <Bernice.Chua.415@gmail.com>
2017-03-30 19:29:31 -0700

    Create PickUpItem.cs & AmmoPickup.cs which inherits from it.

---
+++ Assets/_Second_Version/_Scripts/PickUps/AmmoPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : PickUpItem {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnPickUp(Transform item) {
+        //base.OnPickUp(item);
+        Debug.Log("Inside the public override void OnPickUp(Transform item)....");
+    }
+
+}
