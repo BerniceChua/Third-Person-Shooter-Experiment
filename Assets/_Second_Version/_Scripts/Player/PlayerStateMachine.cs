cb0c4e9ffb7f5112082560c1ee380c12b05a17c8
BerniceChua <Bernice.Chua.415@gmail.com>
2017-04-02 16:47:21 -0700

    Create PlayerStateMachine.cs.

---
+++ Assets/_Second_Version/_Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour {
+
+    public enum EMoveState {
+        WALKING,
+        RUNNING,
+        CROUCHING,
+        SPRINTING
+    }
+
+    public enum EWeaponState {
+        IDLE,
+        FIRING,
+        Aiming,
+        AimFiring
+    }
+
+    public EMoveState m_MoveState;
+    public EWeaponState m_WeaponState;
+
+    private InputController m_inputController;
+    public InputController InputController {
+        get {
+            if (m_inputController == null)
+                m_inputController = GameManager.GameManagerInstance.InputController;
+
+            return m_inputController;
+        }
+    }
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SetMoveState();
+
+    }
+
+    void SetWeaponState() {
+        m_WeaponState = EWeaponState.IDLE;
+
+        if (InputController.m_Fire1)
+            m_WeaponState = EWeaponState.FIRING;
+    }
+
+    void SetMoveState() {
+        m_MoveState = EMoveState.RUNNING;
+
+        if (InputController.m_IsSprinting)
+            m_MoveState = EMoveState.SPRINTING;
+
+        if (InputController.m_IsWalking)
+            m_MoveState = EMoveState.WALKING;
+
+        if (InputController.m_IsCrouched)
+            m_MoveState = EMoveState.CROUCHING;
+    }
+
+}
