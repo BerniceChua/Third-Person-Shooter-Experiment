930311e7e1d5c601232b8ea6d115796c313d002f
BerniceChua <Bernice.Chua.415@gmail.com>
2017-04-05 18:02:53 -0700

    Create logic to handle player's HP & death state.

---
+++ Assets/_Second_Version/_Scripts/Player/PlayerHP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : Destructable {
+
+    [SerializeField] SpawnPoint[] m_spawnPoints;
+
+    void SpawnAtNewSpawnPoint() {
+        int spawnIndex = Random.Range(0, m_spawnPoints.Length);
+        transform.position = m_spawnPoints[spawnIndex].transform.position;
+        transform.rotation = m_spawnPoints[spawnIndex].transform.rotation;
+    }
+
+    public override void Die() {
+        base.Die();
+        SpawnAtNewSpawnPoint();
+    }
+
+    [ContextMenu("Test Die")]
+    void TestDie() {
+        Die();
+    }
+}
