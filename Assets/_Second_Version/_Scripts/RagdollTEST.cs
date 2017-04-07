dfdac21360bf67b5e10fd1435d4a810555b0904b
BerniceChua <Bernice.Chua.415@gmail.com>
2017-04-06 18:59:37 -0700

    Patch over all the glitches of the respawn system.

--- Assets/_Second_Version/_Scripts/RagdollTEST.cs
+++ Assets/_Second_Version/_Scripts/RagdollTEST.cs
@@ -14,7 +14,7 @@ public class RagdollTEST : Destructable {
 	// Use this for initialization
 	void Start () {
         m_bodyParts = transform.GetComponentsInChildren<Rigidbody>();
-        print("m_bodyParts.Length = " + m_bodyParts.Length);
+        //print("m_bodyParts.Length = " + m_bodyParts.Length);
         //if (m_bodyParts.Length == 0)
         //    return;
 
@@ -26,7 +26,6 @@ public class RagdollTEST : Destructable {
 	// Update is called once per frame
 	void Update () {
         if (!IsAlive) {
-            Die();
             return;
         }
 
@@ -35,6 +34,7 @@ public class RagdollTEST : Destructable {
 	}
 
     void SpawnAtNewSpawnPoint() {
+        print("Inside SpawnAtNewSpawnPoint()...");
         int spawnIndex = Random.Range(0, m_spawnPoints.Length);
         transform.position = m_spawnPoints[spawnIndex].transform.position;
         transform.rotation = m_spawnPoints[spawnIndex].transform.rotation;
@@ -46,22 +46,29 @@ public class RagdollTEST : Destructable {
         EnableRagdoll(true);
         m_Animator.enabled = false;
         print("m_Animator.enabled = " + m_Animator.enabled);
+        print("this.gameObject = " + this.gameObject);
+        GameManager.GameManagerInstance.Timer.Add(() => { this.gameObject.SetActive(false); }, 5);
 
         GameManager.GameManagerInstance.Timer.Add(() => {
             EnableRagdoll(false);
-            SpawnAtNewSpawnPoint();
             m_Animator.enabled = true;
             Reset();
+            SpawnAtNewSpawnPoint();
+            //this.gameObject.SetActive(true); 
+            /// This was replaced with the line below, or else 
+            /// the spawned character kept on spawning at each of the spawnpoints.
+            /// It doesn't really solve the issue, it just hides that the glitchy appearance is there.
+            GameManager.GameManagerInstance.Timer.Add(() => { this.gameObject.SetActive(true); }, 5);
         }, 5);
     }
 
     void EnableRagdoll(bool value) {
-        print("Inside EnableRagdoll(bool value)");
-        print("value = " + value);
+        print("Inside EnableRagdoll(" + value + ")");
+        //print("value = " + value);
         for (int i = 0; i < m_bodyParts.Length; i++) {
             m_bodyParts[i].isKinematic = !value;
-            print("m_bodyParts[" + i + "].isKinematic = " + m_bodyParts[i].isKinematic);
-            print("m_bodyParts[i].name = " + m_bodyParts[i].name);
+            //print("m_bodyParts[" + i + "].isKinematic = " + m_bodyParts[i].isKinematic);
+            //print("m_bodyParts[" + i + "].name = " + m_bodyParts[i].name);
         }
     }
 
