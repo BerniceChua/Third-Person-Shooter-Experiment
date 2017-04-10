57bd4df0eaef3ef83ba254c94b361c4193b971c8
BerniceChua <Bernice.Chua.415@gmail.com>
2017-04-09 20:14:57 -0700

    Debug Pathfinding.cs.

--- Assets/_Second_Version/_Shared/WaypointController.cs
+++ Assets/_Second_Version/_Shared/WaypointController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class WaypointController : MonoBehaviour {
 
     //Waypoint[] m_waypoints;
-    Waypoint[] m_waypoints { get { return GetComponentsInChildren<Waypoint>(); } set { m_waypoints = value; } }
+    //Waypoint[] m_waypoints { get { return GetComponentsInChildren<Waypoint>(); } set { m_waypoints = value; } }
+    Waypoint[] m_waypoints { get { return GetWaypoints(); } set { m_waypoints = value; } }
 
     /// <summary>
     ///  This is set to -1 because when the game starts up, it will add 1 to this index.
@@ -20,15 +21,20 @@ public class WaypointController : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
         //m_waypoints = GetComponentsInChildren<Waypoint>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        //m_waypoints = GetWaypoints();
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     public void SetNextWaypoint() {
+        print("Inside SetNextWaypoint(), m_currentWaypointIndex = " + m_currentWaypointIndex);
+
         m_currentWaypointIndex++;
+
+        print("after the ++, m_currentWaypointIndex = " + m_currentWaypointIndex);
 
         if (m_currentWaypointIndex == m_waypoints.Length)
             m_currentWaypointIndex = 0;
@@ -49,10 +55,21 @@ public class WaypointController : MonoBehaviour {
     private void OnDrawGizmos() {
         Gizmos.color = Color.blue;
 
+        Vector3 previousWaypoint = Vector3.zero;
+
         /// Loops through array of waypoints.
-        foreach(Waypoint waypoint in m_waypoints) {
+        //foreach(Waypoint waypoint in m_waypoints) {
+        foreach (Waypoint waypoint in GetWaypoints()) {
+            Vector3 waypointPos = waypoint.transform.position;
+
             /// Draw a sphere on the transform of the waypoint.
-            Gizmos.DrawSphere(waypoint.transform.position, 0.2f);
+            Gizmos.DrawSphere(waypointPos, 0.2f);
+
+            /// Draws lines between each waypoint
+            if (previousWaypoint != Vector3.zero)
+                Gizmos.DrawLine(previousWaypoint, waypointPos);
+
+            previousWaypoint = waypointPos;
         }
     }
 
