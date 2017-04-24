1affce7a870cb82e526b57baac4f4a945c6d734a
BerniceChua <Bernice.Chua.415@gmail.com>
2017-04-23 23:49:24 -0700

    Complete EnemyShoot.cs.

--- Assets/_Second_Version/_Scripts/NPC/EnemyShoot.cs
+++ Assets/_Second_Version/_Scripts/NPC/EnemyShoot.cs
@@ -12,12 +12,52 @@ public class EnemyShoot : WeaponsController {
     //EnemyPlayer m_enemyPlayer;
     EnemyPlayer m_enemyPlayer { get { return GetComponent<EnemyPlayer>(); } set { m_enemyPlayer = value; } }
 
+    bool m_shouldFire;
+
     private void Start() {
         //m_enemyPlayer = GetComponent<EnemyPlayer>();
-        m_enemyPlayer.OnTargetSelected += M_enemyPlayer_OnTargetSelected;
+        m_enemyPlayer.OnTargetSelected += EnemyPlayer_OnTargetSelected;
     }
 
-    private void M_enemyPlayer_OnTargetSelected(Player target) {
+    /// <summary>
+    /// When target is selected, run this function/method
+    /// </summary>
+    /// <param name="target"></param>
+    private void EnemyPlayer_OnTargetSelected(Player target) {
+        ActiveWeapon.m_AimTarget = target.transform;
+        ActiveWeapon.m_AimTargetOffset = Vector3.up * 1.5f;
+    }
+
+    void StartBurst() {
+        if (!m_enemyPlayer.EnemyHealth.IsAlive)
+            return;
+
+        CheckReload();
+        m_shouldFire = true;
+
+        GameManager.GameManagerInstance.Timer.Add(EndBurst, Random.Range(m_burstDurationMin, m_burstDurationMax));
+    }
+
+    void EndBurst() {
+        m_shouldFire = false;
+
+        if (!m_enemyPlayer.EnemyHealth.IsAlive)
+            return;
+
+        CheckReload();
+        GameManager.GameManagerInstance.Timer.Add(StartBurst, m_shootingSpeed);
+    }
+
+    void CheckReload() {
+        if (ActiveWeapon.m_reloader.RoundsRemainingInClip == 0)
+            ActiveWeapon.Reload();
+    }
+
+    private void Update() {
+        if (!m_shouldFire || !m_CanFire || !m_enemyPlayer.EnemyHealth.IsAlive)
+            return;
+
         ActiveWeapon.FireWeapon();
     }
+
 }
