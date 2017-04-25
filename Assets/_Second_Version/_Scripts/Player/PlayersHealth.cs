20e86c16ea9c76c7408c5211557a2c66adf7e56a
BerniceChua <Bernice.Chua.415@gmail.com>
2017-04-24 23:47:31 -0700

    Add PlayersHealth.cs.

---
+++ Assets/_Second_Version/_Scripts/Player/PlayersHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayersHealth : Destructable {
+
+    [SerializeField] Ragdoll m_ragdoll;
+
+    public override void Die() {
+        base.Die();
+
+        m_ragdoll.EnableRagdoll(true);
+    }
+
+}
