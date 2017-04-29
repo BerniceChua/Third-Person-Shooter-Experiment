40e1630bfbfe1c90ce439d3e15c838bd6b96739f
BerniceChua <Bernice.Chua.415@gmail.com>
2017-04-29 16:24:24 -0700

    Create PlayerCover.cs.

---
+++ Assets/_Second_Version/_Scripts/Player/PlayerCover.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCover : MonoBehaviour {
+
+    private bool m_canTakeCover;
+    private bool m_isInCover;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!m_canTakeCover)
+            return;
+
+        if (Input.GetButtonDown("Get Into Cover")) {
+
+        }
+	}
+
+    internal void SetPlayerCoverAllowed(bool value) {
+        m_canTakeCover = value;
+    }
+
+}
