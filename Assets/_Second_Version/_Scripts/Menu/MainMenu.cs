c2ba460c2dd2310f849bf430947cbd4dda107bef
BerniceChua <Bernice.Chua.415@gmail.com>
2017-05-11 19:21:24 -0700

    Create Mainmenu.cs.

---
+++ Assets/_Second_Version/_Scripts/Menu/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string m_LevelName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame() {
+        SceneManager.LoadScene(m_LevelName);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+}
