ec1601dcc6cd6d441927065c9d4398913e9d3601
BerniceChua <Bernice.Chua.415@gmail.com>
2017-05-13 01:17:37 -0700

    Adjust camera so that when player is aiming or is regular, the crosshairs are still aiming at the same place.

---
+++ Assets/_2nd_Version/_Scripts/Player/PlayerAimWithIK.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimWithIK : MonoBehaviour {
+
+    [SerializeField] float m_minAngle;
+    [SerializeField] float m_maxAngle;
+
+    [SerializeField] Transform m_playerDirection;
+    [SerializeField] DynamicCrosshairs m_dynamicCrosshair;
+
+    public void m_SetRotation(float rotationAmount) {
+        float clampedAngle = GetClampedAngle(rotationAmount);
+
+        transform.eulerAngles = new Vector3(clampedAngle, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    private float GetClampedAngle(float rotationAmount) {
+        float newAngle = CheckAngle(transform.eulerAngles.x - rotationAmount);
+        float clampedAngle = Mathf.Clamp(newAngle, m_minAngle, m_maxAngle);
+        return clampedAngle;
+    }
+
+    public float GetAngle() {
+        return CheckAngle(transform.eulerAngles.x);
+    }
+
+    public float CheckAngle(float value) {
+        float angle = value - 180;
+
+        if (angle > 0)
+            return angle - 180;
+
+        return angle + 180;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("test at time " + Time.time);
+        //Debug.Log("Angle = " + GetAngle() + "at" + Time.time);
+	}
+
+    /// <summary>
+    /// This allows the player's gun to aim at the crosshair, not just the projectiles.
+    /// </summary>
+    public void GetCrosshairPosition() {
+        m_playerDirection.LookAt(m_dynamicCrosshair.transform);
+    }
+
+}
