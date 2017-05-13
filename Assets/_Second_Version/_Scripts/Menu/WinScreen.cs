f2dd1f3d60f31962f86f3218b3b6e29f83daa57a
BerniceChua <Bernice.Chua.415@gmail.com>
2017-05-12 20:36:06 -0700

    Update the code for WinScreen.cs.

---
+++ Assets/_Second_Version/_Scripts/Menu/WinScreen.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour {
+
+    [SerializeField] GameObject m_winMessagePanel;
+
+    [SerializeField] Button m_playAgainButton;
+    [SerializeField] Button m_returnToMainMenuButton;
+
+    [SerializeField] PauseAndLockCursor m_cursor;
+
+    //private void Awake() {
+        
+    //}
+
+    // Use this for initialization
+    void Start () {
+        m_winMessagePanel.SetActive(false);
+        GameManager.GameManagerInstance.EventBus.AddListener("OnAllEnemiesKilled", () => {
+            GameManager.GameManagerInstance.m_PlayerIsPaused = true;
+            m_winMessagePanel.SetActive(true);
+        });
+
+        m_playAgainButton.onClick.AddListener(OnPlayAgainClicked);
+        m_returnToMainMenuButton.onClick.AddListener(OnReturnToMainMenuButtonClicked);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_winMessagePanel.activeSelf)
+            return;
+
+		if (GameManager.GameManagerInstance.InputController.m_Escape) {
+            GameManager.GameManagerInstance.m_PlayerIsPaused = true;
+            Cursor.visible = true;
+            //m_cursor.PauseGame();
+            m_winMessagePanel.SetActive(true);
+        }
+	}
+
+    void OnPlayAgainClicked() {
+        SceneManager.LoadScene("Second_Version");
+    }
+
+    void OnReturnToMainMenuButtonClicked() {
+        SceneManager.LoadScene("Second_Version_Main_Menu");
+    }
+
+}
